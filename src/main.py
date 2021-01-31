147babe019fc239099cb67484f0d7a0c76485665
nicknt <nikolasderm@gmail.com>
2021-02-01 00:56:09 +0200

    fix bugs

--- src/main.py
+++ src/main.py
@@ -4,31 +4,37 @@ import numpy as np
 from datetime import datetime
 import random
 import RPi.GPIO as GPIO
-import adafruit_mlx90640  # Import the libraries
+import adafruit_mlx90640
+import socket
+import os
+gw = os.popen("ip -4 route show default").read().split()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect((gw[2], 0))
+IPAddr = s.getsockname()[0]
 
-NickntConstant=1.135 # This is just a number to correct the temprature readings
-THRESH = 60 # This value adjust the brightness (change if mask recognition dont work)
-i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # Setup I2C
-mlx = adafruit_mlx90640.MLX90640(i2c) # Begin MLX90640 with I2C comm
-mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # Set refresh rate
-mlx_shape = (24,32) 
+NickntConstant= 3.5 # value to change
+THRESH = 80 # value to change
+i2c = busio.I2C(board.SCL, board.SDA, frequency=400000) # setup I2C
+mlx = adafruit_mlx90640.MLX90640(i2c) # begin MLX90640 with I2C comm
+mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ # set refresh rate
+mlx_shape = (24,32)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 GPIO.setup(20, GPIO.OUT)
-GPIO.setup(26, GPIO.OUT) # Set Led pins
-frame = np.zeros((24*32,)) # Setup array for storing all 768 temperatures
+GPIO.setup(26, GPIO.OUT)
+frame = np.zeros((24*32,)) # setup array for storing all 768 temperatures
 t_array = []
-def GetTemprature(): # This function returns the highest temprature readind of the thermal camera
+def GetTemprature():
     t1 = time.monotonic()
-    mlx.getFrame(frame) # Read MLX temperatures into frame var
-    data_array = (np.reshape(frame,mlx_shape)) # Reshape to 24x32
+    mlx.getFrame(frame) # read MLX temperatures into frame var
+    data_array = (np.reshape(frame,mlx_shape)) # reshape to 24x32
     t_array.append(time.monotonic()-t1)
     frame.sort()
-    return frame[767] + NickntConstant # This is the final temp plus the constant
+    return int(frame[767] + NickntConstant)
 
 def RefreshHTML():
-    Data = open("/var/www/html/data.txt", "r") # open the data file
-    Datalines = Data.readlines() 
+    Data = open("/var/www/html/data.txt", "r")
+    Datalines = Data.readlines()
     Data.close()
     Table = '<!DOCTYPE html>\n\
     <link rel="stylesheet" href="styleForRaspberry.css">\n\
@@ -40,7 +46,7 @@ def RefreshHTML():
     <th>Mask</th>\n\
     <th>Date</th>\n\
     </tr>\n'
-    for i in range(len(Datalines)): # Change the HTML files dynamicaly depended on the data.txt file
+    for i in range(len(Datalines)):
         Table = Table + "<tr>\n"
         Line = Datalines[i].split(",")
         if Line[0] <= "37":
@@ -61,10 +67,10 @@ def RefreshHTML():
     </body>\n\
     </html>'
     Html = open("/var/www/html/index.html", "w")
-    Html.writelines(Table)# Just write the new html code 
+    Html.writelines(Table)
     Html.close()
 
-def WriteData(x): # This function writes the data into the data.txt file
+def WriteData(x):
     temp = GetTemprature()
     now = datetime.now()
     Time = now.strftime("%d/%m/%Y %H:%M:%S")
@@ -80,41 +86,37 @@ def WriteData(x): # This function writes the data into the data.txt file
 
 faceCascade = cv2.CascadeClassifier('/var/www/html/haarcascade_frontalface_default.xml')
 mouthCascade = cv2.CascadeClassifier('/var/www/html/haarcascade_mcs_mouth.xml')
-eyeCascade = cv2.CascadeClassifier('/var/www/html/haarcascade_eye.xml') # Load the haarcascades for the face recognition
-cap = cv2.VideoCapture(0) # Start WebCam
+eyeCascade = cv2.CascadeClassifier('/var/www/html/haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
 cap.set(3, 640)  # set Width
 cap.set(4, 480)  # set Height
 RefreshHTML()
 Testing = 0
-while True: # Main loop
+while True:
     FaceCounter = 0
     EyesCounter = 0
     MouthCounter = 0
     ret, img = cap.read()
     img = cv2.flip(img, 1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    for i in range(5): # searching for face, eyes, mouth
-       faces = faceCascade.detectMultiScale(
-          gray,
-          scaleFactor=1.2,
-          minNeighbors=5,
-          minSize=(20, 20)
-       )
-       mouths = mouthCascade.detectMultiScale(
-          gray,
-          scaleFactor=1.2,
-          minNeighbors=5,
-          minSize=(20, 20)
-       )
-       eyes = eyeCascade.detectMultiScale(
-          gray,
-          scaleFactor=1.2,
-          minNeighbors=5,
-          minSize=(20, 20)
-       )
-       MouthCounter += len(mouths)
-       FaceCounter += len(faces)
-       EyesCounter += len(eyes)
+    faces = faceCascade.detectMultiScale(
+       gray,
+       scaleFactor=1.2,
+       minNeighbors=5,
+       minSize=(20, 20)
+    )
+    mouths = mouthCascade.detectMultiScale(
+       gray,
+       scaleFactor=1.2,
+       minNeighbors=5,
+       minSize=(20, 20)
+    )
+    eyes = eyeCascade.detectMultiScale(
+       gray,
+       scaleFactor=1.2,
+       minNeighbors=5,
+       minSize=(20, 20)
+    )
     (thresh, black_and_white) = cv2.threshold(gray, THRESH ,255, cv2.THRESH_BINARY)
     facesTest = faceCascade.detectMultiScale(
     black_and_white,
@@ -122,10 +124,10 @@ while True: # Main loop
     minNeighbors=5,
     minSize=(20, 20)
     )
-    MouthCounter = MouthCounter/5
-    FaceCounter = FaceCounter/5
-    EyesCounter = EyesCounter/5
-    if EyesCounter == 0.0 and FaceCounter == 0.0 and len(facesTest) == 0:# Now the WebCam doesn't see anyone and it resets
+    MouthCounter = len(mouths)
+    FaceCounter = len(faces)
+    EyesCounter = len(eyes)
+    if EyesCounter == 0.0 and FaceCounter == 0.0 and len(facesTest) == 0:
         Testing = 0
         GPIO.output(20, GPIO.HIGH)
         GPIO.output(26, GPIO.LOW)
@@ -134,7 +136,7 @@ while True: # Main loop
         Testing = 1
     if Testing == 1:
        LastCheck = 0
-       for x in range(5): # Just dont change it. It works... 
+       for x in range(5):
           ret, img = cap.read()
           img = cv2.flip(img, 1)
           gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -161,4 +163,7 @@ while True: # Main loop
           GPIO.output(20, GPIO.LOW)
           GPIO.output(21, GPIO.HIGH)
     print("Face: " + str(FaceCounter) + "   Eyes: "+str(EyesCounter)+"     Mouth: "+str(MouthCounter)+"   Test: "+str(len(facesTest))+"     "+str(Testing))
-
+    # cv2.imshow('frame', black_and_white)
+    # if cv2.waitKey(1) & 0xFF == ord('q'):
+      #  break
+# cv2.destroyAllWindows()
